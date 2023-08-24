68be3532cb23833facb689f470823f0f7a3e9e0d
SeoJaeMin619 <emiya1663@naver.com>
2023-08-24 10:36:09 +0900

    초기 커밋.

---
+++ algorithm/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Reflection.Metadata.Ecma335;
+
+
+public class Solution
+    {
+        public string solution(int num)
+        {
+            if (num % 2 == 0)
+                return "Even";
+            else
+                return "Odd";
+        }
+    }
