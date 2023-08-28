1add1c3018843d227f51e593defecb8bff5f38aa
SeoJaeMin619 <emiya1663@naver.com>
2023-08-28 10:29:01 +0900

    자릿수 더하기

---
+++ algorithm/자릿수 더하기.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Solution3
+{
+    public int solution(int n)
+    {
+        int answer = 0;
+        //정수 n을 String형으로 변환 후 CharArray로 변환
+        char[] arr = n.ToString().ToCharArray();
+
+        //반복문을 통해 각 해당 문자를 int형으로 바꾸어 answer에 더해줌.
+        foreach (char a in arr)
+        {
+            answer += System.Convert.ToInt32(a.ToString());
+        }
+        return answer;
+    }
+}
