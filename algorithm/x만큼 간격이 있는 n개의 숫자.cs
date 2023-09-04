82ad685ac2609ca4c7c6aceabb90afad17f63a9a
SeoJaeMin619 <emiya1663@naver.com>
2023-09-04 21:26:56 +0900

    x만큼 간격이 있는 n개의 숫자

---
+++ algorithm/x만큼 간격이 있는 n개의 숫자.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Solution6
+{
+    public long[] solution6(int x, int n)
+    {
+        long[] answer = new long[n];
+
+        for (int i = 0; i < n; i++)
+        {
+            if (i == 0)
+                answer[i] = x;
+            else
+                answer[i] = x + answer[i - 1];
+        }
+
+        return answer;
+
+    }
+}
