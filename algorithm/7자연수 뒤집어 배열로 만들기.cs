302e5ab2251fc836ae51f24c089eab416b12a228
SeoJaeMin619 <emiya1663@naver.com>
2023-09-07 20:37:45 +0900

    자연수 뒤집어 배열로 만들기

---
+++ algorithm/7자연수 뒤집어 배열로 만들기.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+public class Solution7
+{
+    public int[] solution7(long n)
+    {
+        string a = n.ToString();
+        int[] answer = new int[a.Length];
+        for (int i = 0; i < a.Length; i++)
+        {
+            answer[i] = (int)(n % 10);
+            n = n / 10;
+        }
+        return answer;
+    }
+}
---
+++ algorithm/8.문자열을 정수로 바꾸기.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Solution8
+{
+    public int solution8(string s)
+    {
+        int answer = int.Parse(s);
+        return answer;
+    }
+}
