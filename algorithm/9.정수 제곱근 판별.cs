d37705f6569feebf1307853e6dbd024488391e5e
SeoJaeMin619 <emiya1663@naver.com>
2023-09-14 19:35:56 +0900

    9. 정수 제곱근 판별

---
+++ algorithm/9.정수 제곱근 판별.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Solution9
+{
+    public long solution9(long n)
+    {
+        long x = (long)Math.Sqrt(n);
+        return (x * x == n) ? (x + 1) * (x + 1) : -1;
+    }
+}
