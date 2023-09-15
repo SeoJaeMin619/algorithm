25fcce8161e5711247e304de7028feb96e136b46
SeoJaeMin619 <emiya1663@naver.com>
2023-09-15 09:34:14 +0900

    10.정수내림차수로정리하기

---
+++ algorithm/10.정수내림차순으로정리하기.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Solution10
+{
+    public long solution10(long n)
+    {
+        char[] arr = n.ToString().ToCharArray();
+        Array.Sort(arr);
+        Array.Reverse(arr);
+        long answer = Convert.ToInt64(new string(arr));
+
+        return answer;
+    }
+}
