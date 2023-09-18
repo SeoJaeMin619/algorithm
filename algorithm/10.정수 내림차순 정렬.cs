2b0663f109852a4362b825bd060b9948051d45d9
SeoJaeMin619 <emiya1663@naver.com>
2023-09-18 10:15:20 +0900

    10.정수 내림차수 정렬 11. 하샤드 수

---
+++ algorithm/10.정수 내림차순 정렬.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+public class so10
+{
+    public long solution10(long n)
+    {
+        char[] arr = n.ToString().ToCharArray();
+        Array.Sort(arr);
+        Array.Reverse(arr);
+        long answer = Convert.ToInt64(new string(arr));
+
+        return answer;
+    }
+}
---
+++ algorithm/11.하샤드 ㅅ ㅜ.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Solution11
+{
+    public bool solution11(int x)
+    {
+        bool answer = true;
+
+        int temp_x = x;
+        int total_sum = 0;
+
+        double length = Math.Truncate(Math.Log10(temp_x));
+
+        int divide = (int)Math.Pow(10, length);
+
+        for (int i = divide; i > 0; i /= 10)
+        {
+            total_sum += (temp_x / i);
+
+            temp_x %= i;
+        }
+
+        answer = x % total_sum == 0 ? true : false;
+
+        return answer;
+    }
+}
+
