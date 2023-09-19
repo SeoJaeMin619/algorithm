f92d96e1d074782a07f4539e831cf10274b43712
SeoJaeMin619 <emiya1663@naver.com>
2023-09-19 20:10:30 +0900

    11. 두 정수 사이의  합

---
+++ algorithm/11.두 정수 사이의 합.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Solutin11
+{
+    public long solution11(int a, int b)
+    {
+        long answer = 0;
+
+        if (a < b)
+        {
+            for (int i = a; i <= b; i++)
+            {
+                answer += i;
+            }
+        }
+        else
+        {
+            for (int i = a; i >= b; i--)
+            {
+                answer += i;
+            }
+        }
+
+        return answer;
+    }
+}
