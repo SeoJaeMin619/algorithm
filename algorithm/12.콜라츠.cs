d8b5bbbbf866518f4f42279d636d0e0dc1fcc7dd
SeoJaeMin619 <emiya1663@naver.com>
2023-09-20 22:36:24 +0900

    콜라츠

---
+++ algorithm/12.콜라츠.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Solution13
+{
+    public int solution(long num)
+    {
+        int answer = 0;
+
+        if (num == 1) return 0;
+
+        for (int i = 0; i < 500; i++)
+        {
+            answer++;
+
+            if (num % 2 == 0) num = num / 2;
+            else num = num * 3 + 1;
+
+            if (num == 1) return answer;
+        }
+        return -1;
+    }
+}
