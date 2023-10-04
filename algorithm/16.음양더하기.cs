f1669a064ccf76784c81ab9018995f59a34a3d21
SeoJaeMin619 <emiya1663@naver.com>
2023-10-04 09:14:51 +0900

    16. 음양 더하기

---
+++ algorithm/16.음양더하기.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+using System;
+
+public class Solution16
+{
+    public int solution(int[] absolutes, bool[] signs)
+    {
+        int answer = 0;
+
+        for (int i = 0; i < absolutes.Length; i++)
+        {
+            if (!signs[i])
+            {
+                absolutes[i] = -absolutes[i];
+            }
+
+            answer += absolutes[i];
+        }
+        return answer;
+    }
+}
