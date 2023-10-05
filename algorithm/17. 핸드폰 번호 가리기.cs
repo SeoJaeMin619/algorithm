6d932d53c919ef02bb784a3d1c909f1e0ce21f7f
SeoJaeMin619 <emiya1663@naver.com>
2023-10-05 11:03:08 +0900

    17. 핸드폰 번호 가리기

---
+++ algorithm/17. 핸드폰 번호 가리기.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+public class Solution17
+{
+    public string solution(string phone_number)
+    {
+        string answer = ""; //결과를 저장할 빈문자열 선언
+
+        string temp_str = phone_number.Substring(phone_number.Length - 4, 4);
+
+        for (int i = 0; i < phone_number.Length - 4; ++i)
+        {
+            answer += "*";
+
+        }
+        answer += temp_str;
+        return answer;
+    }
+}
