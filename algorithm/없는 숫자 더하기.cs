b1ad6eebb7adb03f4c4ae83403b51cb4a963b539
SeoJaeMin619 <emiya1663@naver.com>
2023-10-06 09:29:34 +0900

    없는 숫자 더하기 설명 추가

--- algorithm/없는 숫자 더하기.cs
+++ algorithm/없는 숫자 더하기.cs
@@ -11,16 +11,19 @@ public class Solution18
     {
         int answer = 0;
 
-        for (int i = 0; i < 10; i++)
+        for (int i = 0; i < 10; i++)  //0부터 9까지의 숫자를 반복적으로 처리하는 반복문입니다.
+                                      //i 변수가 0부터 9까지 증가하면서 아래의 코드 블록을 실행합니다.
         {
-            answer += i;
+            answer += i; //0부터 9까지 숫자를 더한 결과가 answer변수에 저장
         }
 
-        foreach (var data in numbers)
+        foreach (var data in numbers) //배열 numners를 반복적으로 처리하는 반복분
+                                      //var data는 배열 numbers의 각 요소를 나타내는 변수
+
         {
-            answer -= data;
+            answer -= data; //answer 변수에서 data값을 뺌
         }
 
-        return answer;
+        return answer;  //0부터 9까지의 숫자를 모두 더한 후, 배열 numbers의 모든 요소를 뺀 결과
     }
 }
